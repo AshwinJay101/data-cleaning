5c8667218941a57568bda6bd939b19b379f6b7b7
Ashwin Jayaraman <ashwin@Ashwins-MacBook-Pro.local>
2019-02-21 22:44:33 -0500

    Basic Skeleton Code

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
 from data_test.data_tester import data_test
 
-
+if __name__ == '__main__':
+    path_to_data = 'test/data/test_data_1.csv'
+    tester = data_test(path_to_data)
+    print(tester.see_data())
--- data_test/data_tester.py
+++ data_test/data_tester.py
@@ -6,3 +6,6 @@ class data_test:
     def __init__(self, path_of_data=''):
         self.path = path_of_data
         self.data = pd.read_csv(self.path)
+
+    def see_data(self, no_of_rows = 1):
+        return self.data.head(no_of_rows)
